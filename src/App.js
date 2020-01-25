d6de64060ade972788d614e37f0c64e1efc717b3
Lisa Felder <feldmuesle@yahoo.com>
2020-01-25 12:12:12 +0100

    basic form setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [inputValue, setInputValue] = useState('')
+
+  function handleChange(event) {
+    const value = event.target.value
+    setInputValue(value)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log('handle submit')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label htmlFor="username">Github user name</label>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          onChange={handleChange}
+          value={inputValue}
+          placeholder="please enter a valid Github user"
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
