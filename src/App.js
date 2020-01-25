7298f403c1b712d6beafa6da618c564de1761b70
Lisa Felder <feldmuesle@yahoo.com>
2020-01-25 14:45:20 +0100

    basic hook up apollo with react/proof of concept

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { gql } from 'apollo-boost';
+
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_USER = gql`
+  {
+  user(login: "feldmuesle") {
+    name
+    bio
+    url
+    company
+    resourcePath
+    repositories(first: 10) {
+      edges {
+        node {
+          id
+          name
+          isPrivate
+          updatedAt
+          url
+        }
+      }
+      totalCount
+    }
+    websiteUrl
+    viewerCanCreateProjects
+  }
+}
+`;
 
 function App() {
+
+  const { loading, error, data } = useQuery(GET_USER)
+
+  console.log('response:', loading, error, data)
 
   const [inputValue, setInputValue] = useState('')
 
@@ -17,19 +50,19 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <label htmlFor="username">Github user name</label>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          onChange={handleChange}
-          value={inputValue}
-          placeholder="please enter a valid Github user"
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+      <div className="App">
+        <label htmlFor="username">Github user name</label>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            onChange={handleChange}
+            value={inputValue}
+            placeholder="please enter a valid Github user"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
 
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
@@ -16,22 +18,12 @@ const client = new ApolloClient({
   },
 });
 
-const GET_USER = gql`
-  {
-    viewer {
-      name
-      url
-    }
-  }
-`;
-
-
-client
-  .query({
-    query: GET_USER
-  }).then(console.log)
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
